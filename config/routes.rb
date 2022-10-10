be4918e2b9b9c4d54c495e5e5679be16232842f8
hajame <harri.mehtala@helsinki.fi>
2022-10-10 15:49:03 +0300

    Add ratings route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'kaikki_bisset', to: 'beers#index'
+  get 'ratings', to: 'ratings#index'
   # Defines the root path route ("/")
   root "breweries#index"
 end
-:
