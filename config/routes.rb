008bc126430934bd0cce83892a76c8549f9acfe2
hajame <harri.mehtala@helsinki.fi>
2022-10-10 15:37:50 +0300

    Add index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :beers
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'kaikki_bisset', to: 'beers#index'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "breweries#index"
 end
+:
