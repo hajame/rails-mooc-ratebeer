2e7465e7c865e2b0898826b9479873ebc729063e
hajame <harri.mehtala@helsinki.fi>
2022-10-11 15:57:04 +0300

    W2 E5 Count avg ratings using 3 different methods

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -3,6 +3,12 @@ class Beer < ApplicationRecord
     has_many :ratings
 
     def average_rating
+
+        # Counting the average with ActiveRecord::Calculations
+        # =>  ratings.average(:score).to_f
+        #
+        # 'sum' can be calculated also with reduce:
+        # =>  ratings.reduce(0) { |sum, rating| sum + rating.score }
         sum = ratings.map(&:score).sum
         sum.to_f / ratings.size
     end
