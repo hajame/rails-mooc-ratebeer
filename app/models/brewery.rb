e4c9312b52d7b5444897ff982f6c89bd3740d138
hajame <harri.mehtala@helsinki.fi>
2022-10-11 14:49:12 +0300

    Add experimental methods to brewery model

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,14 @@
 class Brewery < ApplicationRecord
     has_many :beers
+
+    def print_report
+        puts name
+        puts "established at year #{year}"
+        puts "number of beers #{beers.count}"
+    end
+
+    def restart
+        self.year = 2022
+        puts "changed year to #{year}"
+    end
 end
