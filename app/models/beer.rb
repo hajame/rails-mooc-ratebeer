616ff4187a9604352cc217f027722024efa2a329
hajame <harri.mehtala@helsinki.fi>
2022-10-11 15:13:20 +0300

    W2 E4: Add beer average rating, Add Beer and Brewery links

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,9 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        sum = ratings.map(&:score).sum
+        sum.to_f / ratings.size
+    end
 end
