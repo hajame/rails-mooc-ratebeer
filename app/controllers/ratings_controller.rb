d080214f76311251774c1e03aa7fe60689388ad7
hajame <harri.mehtala@helsinki.fi>
2022-10-15 20:03:16 +0300

    Create new rating, add debugger config

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,4 +3,14 @@ class RatingsController < ApplicationController
   def index
     @ratings = Rating.all
   end
+
+  def new
+    @rating = Rating.new
+  end
+
+  def create
+    Rating.create params.require(:rating).permit(:beer_id, :score)
+    redirect_to ratings_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   resources :breweries
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'kaikki_bisset', to: 'beers#index'
+
   get 'ratings', to: 'ratings#index'
+  post 'ratings', to: 'ratings#create'
+
+  get 'ratings/new', to: 'ratings#new'
+
   # Defines the root path route ("/")
   root "breweries#index"
 end
