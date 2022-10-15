561c337bf683ee71513d9a3afb5b0f2d4c625875
hajame <harri.mehtala@helsinki.fi>
2022-10-15 21:12:42 +0300

    W2 E10: Add style dropdown for beer creation

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -14,6 +14,7 @@ class BeersController < ApplicationController
   def new
     @beer = Beer.new
     @breweries = Brewery.all
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter", "Lowalcohol"]
   end
 
   # GET /beers/1/edit
