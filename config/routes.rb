939471f726d06f3fb4b9b185e5ef80d6e2e972d7
hajame <harri.mehtala@helsinki.fi>
2022-10-25 10:57:12 +0300

    Use resource routing for ratings, fixing path for new rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   resources :beers
   resources :breweries
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'kaikki_bisset', to: 'beers#index'
 
-  get 'ratings', to: 'ratings#index'
-  post 'ratings', to: 'ratings#create'
-
-  get 'ratings/new', to: 'ratings#new'
+  resources :ratings, only: [:index, :new, :create]
 
   # Defines the root path route ("/")
   root "breweries#index"
