ed7cd2f1bb037ec3a2f3bd0f7e04f21c5d549ab8
hajame <harri.mehtala@helsinki.fi>
2022-10-28 09:33:09 +0300

    W2 E14: Include rating count and average on Brewery view

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,10 @@
 class Brewery < ApplicationRecord
     has_many :beers, dependent: :destroy
+    has_many :ratings, through: :beers
+
+    def average_rating
+        ratings.average(:score).to_f
+    end
 
     def print_report
         puts name
