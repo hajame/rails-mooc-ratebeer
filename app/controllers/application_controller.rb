91a8decd9201865867cd29b64fc6ff1871299ddc
hajame <harri.mehtala@helsinki.fi>
2022-11-04 16:26:49 +0200

    Add view helper method for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    return nil if session[:user_id].nil?
+    User.find(session[:user_id])
+  end
 end
