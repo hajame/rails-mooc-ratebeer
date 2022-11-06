71ce9b658b25ba1ae5d65178a083e40c64bd3182
hajame <harri.mehtala@helsinki.fi>
2022-11-06 17:59:24 +0200

    Handle incorrect username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     return nil if session[:user_id].nil?
+
     User.find(session[:user_id])
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    session[:user_id] = user.id if user
-    redirect_to user
+    if user.nil?
+      redirect_to signin_path, notice: "User #{params[:username]} does not exist!"
+    else
+      session[:user_id] = user.id if user
+      redirect_to user, notice: "Welcome back!"
+    end
   end
 
   def destroy
