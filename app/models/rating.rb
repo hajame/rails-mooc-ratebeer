2cff8418f1516f30751bb57a15e7879666804f7b
hajame <harri.mehtala@helsinki.fi>
2022-11-06 18:52:02 +0200

    Add Rating constraints

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,6 +2,10 @@ class Rating < ApplicationRecord
   belongs_to :beer
   belongs_to :user
 
+  validates :score, numericality: { greater_than_or_equal_to: 1,
+                                    less_than_or_equal_to: 50,
+                                    only_integer: true }
+
   def to_s
     "#{beer.name}: #{score}"
   end
