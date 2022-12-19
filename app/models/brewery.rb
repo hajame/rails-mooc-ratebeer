a482859dd8f28a1bb869372d2877f342f51fbbfc
hajame <harri.mehtala@helsinki.fi>
2022-12-19 15:02:10 +0200

    W3 E7: New brewery year must be btwn 1040-2022

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -4,6 +4,10 @@ class Brewery < ApplicationRecord
   has_many :beers, dependent: :destroy
   has_many :ratings, through: :beers
 
+  validates :year, numericality: { greater_than_or_equal_to: 1040,
+                                   less_than_or_equal_to: 2022,
+                                   only_integer: true }
+
   def print_report
     puts name
     puts "established at year #{year}"
