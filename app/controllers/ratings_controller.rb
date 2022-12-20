ca6f87557acf8a4792a00f12671c1217ae515d6d
hajame <harri.mehtala@helsinki.fi>
2022-12-20 14:58:06 +0200

    User can only delete their own ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -23,7 +23,7 @@ class RatingsController < ApplicationController
 
   def destroy
     rating = Rating.find(params[:id])
-    rating.delete
-    redirect_to ratings_path
+    rating.delete if current_user == rating.user
+    redirect_to user_path(current_user)
   end
 end
