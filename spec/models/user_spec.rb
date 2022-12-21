4314db1c969d5ce133afd511b5afbadf6d21bd44
hajame <harri.mehtala@helsinki.fi>
2022-12-21 14:00:26 +0200

    W4: more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,33 @@ RSpec.describe User, type: :model do
 
     expect(user.username).to eq("Pekka")
   end
+
+  it "is not saved without a password" do
+    user = User.new username: "Pekka"
+
+    expect(user).not_to be_valid
+    expect(User.count).to eq(0)
+  end
+
+  describe "with a proper password" do
+    let(:user) { User.create username: "Pekka", password: "Secret1", password_confirmation: "Secret1" }
+    let(:test_brewery) { Brewery.new name: "test", year: 2000 }
+    let(:test_beer) { Beer.create name: "testbeer", style: "teststyle", brewery: test_brewery }
+
+    it "is saved" do
+      expect(user.valid?).to be(true)
+      expect(User.count).to eq(1)
+    end
+
+    it "with a proper password and two ratings, has the correct average rating" do
+      rating = Rating.new score: 10, beer: test_beer
+      rating2 = Rating.new score: 20, beer: test_beer
+
+      user.ratings << rating
+      user.ratings << rating2
+
+      expect(user.ratings.count).to eq(2)
+      expect(user.average_rating).to eq(15.0)
+    end
+  end
 end
