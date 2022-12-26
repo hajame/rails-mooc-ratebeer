c267a5bcbe404bfe49305ef6e5f73b8d139a8d16
hajame <harri.mehtala@helsinki.fi>
2022-12-26 16:46:44 +0200

    W4 Add signin system tests

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -12,9 +12,29 @@ describe "User" do
       fill_in('password', with: 'Foobar1')
       click_button('Log in')
 
-      save_and_open_page
       expect(page).to have_content 'Welcome back!'
       expect(page).to have_content 'Pekka'
+    end
+
+    it "is redirected back to signin form if wrong credentials given" do
+      visit signin_path
+      fill_in('username', with: 'Pekka')
+      fill_in('password', with: 'wrong')
+      click_button "Log in"
+
+      expect(current_path).to eq(signin_path)
+      expect(page).to have_content "Username and/or password mismatch"
+    end
+
+    it "when signed up with good credentials, is added to the system" do
+      visit signup_path
+      fill_in 'user_username', with: 'Brian'
+      fill_in 'user_password', with: 'Secret55'
+      fill_in 'user_password_confirmation', with: 'Secret55'
+
+      expect {
+        click_button('Create User')
+      }.to change { User.count }.by(1)
     end
   end
 end
