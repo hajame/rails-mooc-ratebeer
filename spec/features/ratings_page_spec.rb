b20bb0993a0bf1009e1e627a7f500b702223a439
hajame <harri.mehtala@helsinki.fi>
2022-12-27 13:04:45 +0200

    W4 E6 Ratings page content system test

--- spec/features/ratings_page_spec.rb
+++ spec/features/ratings_page_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 include Helpers
 
-describe "Rating" do
-  let!(:brewery) { FactoryBot.create :brewery, name: "koff" }
+describe "Ratings page" do
+  let!(:brewery) { FactoryBot.create :brewery }
   let!(:beer1) { FactoryBot.create :beer, name: "iso 3", brewery: brewery }
   let!(:beer2) { FactoryBot.create :beer, name: "Karhu", brewery: brewery }
   let!(:user) { FactoryBot.create :user }
@@ -12,17 +12,30 @@ describe "Rating" do
     sign_in username: "Pekka", password: "Foobar1"
   end
 
-  it "when given, is registered to the beer and user who is signed in" do
-    visit new_rating_path
-    select('iso 3', from: 'rating[beer_id]')
-    fill_in 'rating[score]', with: '15'
+  it "contains a list of ratings, and their count" do
+    FactoryBot.create :rating, user: user, beer: beer1
+    FactoryBot.create :rating, user: user, beer: beer2
 
-    expect {
-      click_button "Create Rating"
-    }.to change { Rating.count }.from(0).to(1)
+    visit ratings_path
 
-    expect(user.ratings.count).to eq(1)
-    expect(beer1.ratings.count).to eq(1)
-    expect(beer1.average_rating).to eq(15.0)
+    expect(page).to have_content "Number of ratings: 2"
+    expect(page).to have_content "#{beer1.name} 10 Pekka"
+    expect(page).to have_content "#{beer2.name} 10 Pekka"
+  end
+
+  describe "New rating" do
+    it "when created, is registered to the beer and user who is signed in" do
+      visit new_rating_path
+      select('iso 3', from: 'rating[beer_id]')
+      fill_in 'rating[score]', with: '15'
+
+      expect {
+        click_button "Create Rating"
+      }.to change { Rating.count }.from(0).to(1)
+
+      expect(user.ratings.count).to eq(1)
+      expect(beer1.ratings.count).to eq(1)
+      expect(beer1.average_rating).to eq(15.0)
+    end
   end
 end
