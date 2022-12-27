95c2237cab2efd2ef358eb1cf4ba2952f324663a
hajame <harri.mehtala@helsinki.fi>
2022-12-27 13:58:14 +0200

    W4 E8 User deletes ratings system test

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 include Helpers
 
-describe "User page" do
+describe "User" do
   let!(:user) { FactoryBot.create :user }
 
-  describe "when user has signed up signed up" do
+  describe "who has signed up signed up" do
     it "can signin with right credentials" do
       sign_in username: "Pekka", password: "Foobar1"
 
@@ -31,10 +31,15 @@ describe "User page" do
       }.to change { User.count }.by(1)
     end
 
-    describe "when navigating on a User page" do
+    describe "User page" do
+      let!(:beer1) { FactoryBot.create :beer, name: "iso 3" }
+      let!(:beer2) { FactoryBot.create :beer, name: "Karhu" }
+
+      before :each do
+        sign_in username: "Pekka", password: "Foobar1"
+      end
+
       it "Sees only ratings done by that user, and no one else" do
-        beer1 = FactoryBot.create :beer, name: "iso 3"
-        beer2 = FactoryBot.create :beer, name: "Karhu"
         user2 = FactoryBot.create :user, username: "Angela"
         FactoryBot.create :rating, user: user, beer: beer1, score: 5
         FactoryBot.create :rating, user: user2, beer: beer2, score: 10
@@ -46,6 +51,19 @@ describe "User page" do
         visit user_path(user2)
         expect(page).to have_content "Karhu 10"
         expect(page).not_to have_content "iso 3 5"
+      end
+
+      it "Can delete one's own ratings" do
+        FactoryBot.create :rating, user: user, beer: beer1, score: 5
+        FactoryBot.create :rating, user: user, beer: beer2, score: 10
+
+        visit user_path(user)
+        expect(page).to have_content "iso 3 5"
+        expect(page).to have_content "Karhu 10"
+
+        click_on "button_delete_rating_1"
+        expect(page).not_to have_content "iso 3 5"
+        expect(page).to have_content "Karhu 10"
       end
     end
   end
