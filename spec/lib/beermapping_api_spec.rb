09fb58f44fb95258e600d8284b53b1203f94e1c2
hajame <harri.mehtala@helsinki.fi>
2022-12-29 11:18:39 +0200

    W5 E3 Beermapping API returns array of Places

--- spec/lib/beermapping_api_spec.rb
+++ spec/lib/beermapping_api_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 describe "BeermappingApi" do
+  it "When HTTP GET returns empty, an empty array is returned" do
+    canned_answer = <<-END_OF_STRING
+<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id></id><name></name><status></status><reviewlink></reviewlink><proxylink></proxylink><blogmap></blogmap><street></street><city></city><state></state><zip></zip><country></country><phone></phone><overall></overall><imagecount></imagecount></location></bmp_locations>
+    END_OF_STRING
+
+    stub_request(:get, /.*veinrahas/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+    places = BeermappingApi.places_in("veinrahas")
+    expect(places.size).to eq(0)
+  end
+
   it "When HTTP GET returns one entry, it is parsed and returned" do
 
     canned_answer = <<-END_OF_STRING
@@ -17,14 +27,24 @@ describe "BeermappingApi" do
     expect(place.street).to eq("Eerikinkatu 18")
   end
 
-  it "When HTTP GET returns empty, an empty array is returned" do
+  it "When HTTP GET returns two entries, an array of Places is returned" do
+
     canned_answer = <<-END_OF_STRING
-<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id></id><name></name><status></status><reviewlink></reviewlink><proxylink></proxylink><blogmap></blogmap><street></street><city></city><state></state><zip></zip><country></country><phone></phone><overall></overall><imagecount></imagecount></location></bmp_locations>
+<?xml version='1.0' encoding='utf-8' ?><bmp_locations><location><id>21528</id><name>Maistila</name><status>Brewery</status><reviewlink>https://beermapping.com/location/21528</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=21528&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=21528&amp;d=1&amp;type=norm</blogmap><street>Kaarnatie 20</street><city>Oulu</city><state>Oulun Laani</state><zip>90530</zip><country>Finland</country><phone>044 291 9589</phone><overall>0</overall><imagecount>0</imagecount></location><location><id>21547</id><name>Sonnisaari</name><status>Brewery</status><reviewlink>https://beermapping.com/location/21547</reviewlink><proxylink>http://beermapping.com/maps/proxymaps.php?locid=21547&amp;d=5</proxylink><blogmap>http://beermapping.com/maps/blogproxy.php?locid=21547&amp;d=1&amp;type=norm</blogmap><street>Tuotekuja 1</street><city>Oulu</city><state>Oulun Laani</state><zip>90410</zip><country>Finland</country><phone></phone><overall>0</overall><imagecount>0</imagecount></location></bmp_locations>
     END_OF_STRING
 
-    stub_request(:get, /.*veinrahas/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
-    places = BeermappingApi.places_in("veinrahas")
-    expect(places.size).to eq(0)
+    stub_request(:get, /.*oulu/).to_return(body: canned_answer, headers: { 'Content-Type' => "text/xml" })
+
+    places = BeermappingApi.places_in("oulu")
+    expect(places.size).to eq(2)
+
+    expect(places.first.is_a?(Place)).to be(true)
+    expect(places.first.name).to eq("Maistila")
+    expect(places.first.street).to eq("Kaarnatie 20")
+
+    expect(places.last.is_a?(Place)).to be(true)
+    expect(places.last.name).to eq("Sonnisaari")
+    expect(places.last.street).to eq("Tuotekuja 1")
   end
 
 end
