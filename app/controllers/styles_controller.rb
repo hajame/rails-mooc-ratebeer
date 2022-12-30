05ad98ed02c633fb50b222d58a7b4a2618084dea
hajame <harri.mehtala@helsinki.fi>
2022-12-30 20:37:29 +0200

    W5 E10-13 Authentication + more linkage

--- app/controllers/styles_controller.rb
+++ app/controllers/styles_controller.rb
@@ -1,4 +1,5 @@
 class StylesController < ApplicationController
+  before_action :ensure_that_signed_in, except: [:index, :show]
   before_action :set_style, only: %i[show edit update destroy]
 
   # GET /styles or /styles.json
