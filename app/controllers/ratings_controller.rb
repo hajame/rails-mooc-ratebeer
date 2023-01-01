a9706a885e540b58a2ebfc0bc17b4a0bdc57790a
hajame <harri.mehtala@helsinki.fi>
2023-01-01 11:18:41 +0200

    W6 E8 Styles top 3

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,6 +4,7 @@ class RatingsController < ApplicationController
     @ratings = Rating.all
     @top_breweries = Brewery.top(3)
     @top_beers = Beer.top(3)
+    @top_styles = Style.top(3)
     @recent = Rating.recent
   end
 
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,3 +1,10 @@
 class Style < ApplicationRecord
   has_many :beers, dependent: :destroy
+  has_many :ratings, through: :beers
+
+  include RatingAverage
+
+  def self.top(limit)
+    Style.all.sort_by { |s| -s.average_rating }.take(limit)
+  end
 end
