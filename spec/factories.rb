643527c29673001abf3e0eb454a0a8bf05f229cb
hajame <harri.mehtala@helsinki.fi>
2023-01-04 11:04:29 +0200

    W6 E13 All tests are passing, adding new fields to user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,6 +3,8 @@ FactoryBot.define do
     username { "Pekka" }
     password { "Foobar1" }
     password_confirmation { "Foobar1" }
+    admin { false }
+    closed { false }
   end
 
   factory :style do
