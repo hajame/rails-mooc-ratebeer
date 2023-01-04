1528b856310de730addb2fe83887d9a2b7652591
hajame <harri.mehtala@helsinki.fi>
2023-01-04 09:03:14 +0200

    W6 Helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def edit_and_destroy_buttons(item)
+    return if current_user.nil?
+
+    edit = link_to('Edit', url_for([:edit, item]), class: "btn btn-primary")
+    del = link_to('Destroy', item, method: :delete,
+                                   form: { data: { turbo_confirm: "Are you sure ?" } },
+                                   class: "btn btn-danger")
+    raw("#{edit} #{del}")
+  end
 end
