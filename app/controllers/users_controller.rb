9160b58b5ff75504adee0d171b86ee3cb1b8bf3f
hajame <harri.mehtala@helsinki.fi>
2023-01-04 10:38:52 +0200

    W6 E11-12 Hide button from non-admins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
+  before_action :ensure_that_is_admin, only: [:toggle_closed]
 
   # GET /users or /users.json
   def index
