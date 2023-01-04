a3d1c886341b91a94eefe09f0972d4fc8f30452c
hajame <harri.mehtala@helsinki.fi>
2023-01-04 13:05:10 +0200

    W6 E14-16 Avoid session fixation: reset_session on login   - Rails seems to already do this, not sure

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(username: params[:username])
     if user&.authenticate(params[:password]) && !user.closed
+      reset_session
       session[:user_id] = user.id
+      session[:hello] = user.id
       redirect_to user_path(user), notice: "Welcome back!"
     elsif user&.closed
       redirect_to signin_path, alert: "Your account is locked. Contact Administration."
