f0318266c73fe25e2c51a2f15879ec7cf97552f3
hajame <harri.mehtala@helsinki.fi>
2023-01-05 10:27:46 +0200

    W7 Beer List sorting with JS

--- app/javascript/custom/utils.js
+++ app/javascript/custom/utils.js
@@ -5,12 +5,9 @@ const handleResponse = (beers) => {
   BEERS.show()
 }
 
-BEERS.reverse = () => {
-  BEERS.list.reverse()
-}
-
 const createTableRow = (beer) => {
   const tr = document.createElement("tr")
+  tr.classList.add("tablerow")
   const name = tr.appendChild(document.createElement("td"))
   name.innerHTML = beer.name
   const style = tr.appendChild(document.createElement("td"))
@@ -22,6 +19,7 @@ const createTableRow = (beer) => {
 }
 
 BEERS.show = () => {
+  document.querySelectorAll(".tablerow").forEach((el) => el.remove())
   const table = document.getElementById("beerlist")
 
   BEERS.list.forEach((beer) => {
@@ -30,7 +28,45 @@ BEERS.show = () => {
   })
 }
 
+BEERS.sortByName = () => {
+  BEERS.list.sort((a, b) => {
+    return a.name.toUpperCase().localeCompare(b.name.toUpperCase())
+  })
+}
+
+BEERS.sortByStyle = () => {
+  BEERS.list.sort((a, b) => {
+    return a.style.beer_type.toUpperCase().localeCompare(b.name.toUpperCase())
+  })
+}
+
+BEERS.sortByBrewery = () => {
+  BEERS.list.sort((a, b) => {
+    return a.brewery.name.toUpperCase().localeCompare(b.brewery.name.toUpperCase())
+  })
+}
+
 const beers = () => {
+  if (document.querySelectorAll("#beertable").length < 1) return
+
+  document.getElementById("name").addEventListener("click", (e) => {
+    e.preventDefault()
+    BEERS.sortByName()
+    BEERS.show()
+  })
+
+  document.getElementById("style").addEventListener("click", (e) => {
+    e.preventDefault()
+    BEERS.sortByStyle()
+    BEERS.show()
+  })
+
+  document.getElementById("brewery").addEventListener("click", (e) => {
+    e.preventDefault()
+    BEERS.sortByBrewery()
+    BEERS.show()
+  })
+
   fetch("beers.json")
     .then((response) => response.json())
     .then(handleResponse)
