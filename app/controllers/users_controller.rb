717189d227926c149fd606808536ecf9c0008dd8
hajame <harri.mehtala@helsinki.fi>
2023-01-05 19:43:29 +0200

    W7 E9 With 6 users, reduced SQL queries from 14 to 5. O(1) queries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.includes(:ratings).all
   end
 
   # GET /users/1 or /users/1.json
