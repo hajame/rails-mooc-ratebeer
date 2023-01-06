94331dc51323035f70e4416f3a8a738c01f35d97
hajame <harri.mehtala@helsinki.fi>
2023-01-06 10:23:03 +0200

    W7 delete unused field

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -3,12 +3,11 @@ class RatingsController < ApplicationController
 
   # GET /ratings or /ratings.json
   def index
-    @ratings = Rating.all
-    @top_breweries = Brewery.top(3)
+    @recent = Rating.recent
     @top_beers = Beer.top(3)
     @top_styles = Style.top(3)
+    @top_breweries = Brewery.top(3)
     @top_users = User.top(3)
-    @recent = Rating.recent
   end
 
   def new
